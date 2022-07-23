48289600a8c728240c38480dd83a051e818536fc
Khafid Prayoga <khafidp@pm.me>
2022-07-23 21:56:06 +0700

    refactoring

--- client/main.go
+++ client/main.go
@@ -1,24 +1,34 @@
 package main
 
-func main() {
-	// conn, err := grpc.Dial("localhost:4500",
-	// 	grpc.WithTransportCredentials(insecure.NewCredentials()))
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// client := pb.NewStudentServiceClient(conn)
+import (
+	"context"
+	"fmt"
+	"time"
 
-	// student := pb.CreateStudentRequest{
-	// 	FullName:  "Khafid Prayoga",
-	// 	Address:   "Mojokerto",
-	// 	Class:     "X-TKJ-2",
-	// 	BirthDate: uint64(time.Now().Unix()),
-	// }
-	// created, err := client.CreateStudent(context.Background(), &student)
-	// if err != nil {
-	// 	fmt.Printf("error %v", err)
-	// }
-	// fmt.Printf("Data student\n\n%v \n", created)
+	"github.com/khafidprayoga/grpc-basic/proto/pb"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost:4500",
+		grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		panic(err)
+	}
+	client := pb.NewStudentServiceClient(conn)
+
+	student := pb.CreateStudentRequest{
+		FullName:  "Khafid Prayoga",
+		Address:   "Mojokerto",
+		Class:     "X-TKJ-2",
+		BirthDate: uint64(time.Now().Unix()),
+	}
+	created, err := client.CreateStudent(context.Background(), &student)
+	if err != nil {
+		fmt.Printf("error %v", err)
+	}
+	fmt.Printf("Data student\n\n%v \n", created)
 	// data, _ := client.GetStudentById(context.Background(), &pb.GetStudentByIdRequest{Id: 1})
 	// fmt.Printf("Data student\n\n%v \n", data)
 
--- controller/data_student.go
+++ controller/data_student.go
@@ -4,12 +4,30 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/khafidprayoga/grpc-basic/common/entities"
 	"github.com/khafidprayoga/grpc-basic/proto/pb"
 	"google.golang.org/protobuf/types/known/emptypb"
 	"gorm.io/gorm"
 )
+
+func AddStudent(db *gorm.DB, req *pb.CreateStudentRequest) (*pb.CreatedStudentResponse, error) {
+	student := entities.Student{
+		FullName:  req.GetFullName(),
+		BirthDate: time.Unix(int64(req.GetBirthDate()), 0),
+		Class:     req.GetClass(),
+		Address:   req.GetAddress(),
+	}
+	if err := db.Create(&student).Error; err != nil {
+		return nil, errors.New("Gagal menambahkan murid baru")
+	}
+
+	result := pb.CreatedStudentResponse{
+		Id: int64(student.Id),
+	}
+	return &result, nil
+}
 
 func GetStudentById(db *gorm.DB, req *pb.GetStudentByIdRequest) (*pb.StudentDetailResponse, error) {
 	var student entities.Student
